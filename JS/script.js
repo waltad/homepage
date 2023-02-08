2c804cac16703f297f393a76f182222dfb9e47e3
Tadek Waluga <tadek.waluga@gmail.com>
2023-02-08 21:37:42 +0100

    rename class section__header and file my_photo.jpg

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,13 @@
 console.log("Hello! How are you?");
 
 let buttonHeader = document.querySelector(".js-buttonHeader");
-let header1 = document.querySelector(".js-header1");
+let header = document.querySelector(".js-header");
 let buttonTheme = document.querySelector(".js-buttonTheme");
 let body = document.querySelector(".body");
-// let dark = document.querySelector(".body__dark");
 let changeTheme = document.querySelector(".js-changeTheme");
 
 buttonHeader.addEventListener("click", () => {
-    header1.remove();
+    header.remove();
 });
 
 buttonTheme.addEventListener("click", () => {
@@ -16,8 +15,3 @@ buttonTheme.addEventListener("click", () => {
 
     changeTheme.innerText = body.classList.contains("body__dark") ? "dzienny" : "nocny";
 });
-    // if (body.classList.contains("bodyDark")) {
-    //     changeTheme.innerText = "dzienny";
-    // } else {
-    //     changeTheme.innerText = "nocny";
-    // }
