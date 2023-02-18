fb2cb51861f152e629ff044cd450e931d7fac4fd
Tadek Waluga <tadek.waluga@gmail.com>
2023-02-18 20:55:30 +0100

    split file syle.css

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
     const welcome = () => {
         console.log("Hello! How are you?");
     }
-    
+
     const toggleTheme = () => {
         const body = document.querySelector(".body");
         const changeTheme = document.querySelector(".js-changeTheme");
@@ -14,17 +14,17 @@
         const header = document.querySelector(".js-header");
         header.remove()
     }
-    
-    const init = () => {
-        const buttonHeader = document.querySelector(".js-buttonHeader");        
-        const buttonTheme = document.querySelector(".js-buttonTheme");
-            
-        buttonHeader.addEventListener("click", removeHeader);    
-        buttonTheme.addEventListener("click", toggleTheme);
-            
-        welcome();
-    }         
 
-init ();
+    const init = () => {
+        const buttonHeader = document.querySelector(".js-buttonHeader");
+        const buttonTheme = document.querySelector(".js-buttonTheme");
+
+        buttonHeader.addEventListener("click", removeHeader);
+        buttonTheme.addEventListener("click", toggleTheme);
+
+        welcome();
+    }
+
+    init();
 
 }
