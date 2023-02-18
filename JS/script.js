5d7711a22208c4fe82fe47235b28f0198c686159
Tadek Waluga <tadek.waluga@gmail.com>
2023-02-18 17:06:23 +0100

    wrapping the js code in a block and functions

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,26 @@
-console.log("Hello! How are you?");
+{
+    const welcome = () => {
+        console.log("Hello! How are you?");
+    }
+    
+    const onChangeThemeClic = () => {
+        body.classList.toggle("body__dark");
+        changeTheme.innerText = body.classList.contains("body__dark") ? "dzienny" : "nocny";
+    }
 
-let buttonHeader = document.querySelector(".js-buttonHeader");
-let header = document.querySelector(".js-header");
-let buttonTheme = document.querySelector(".js-buttonTheme");
-let body = document.querySelector(".body");
-let changeTheme = document.querySelector(".js-changeTheme");
-
-buttonHeader.addEventListener("click", () => {
-    header.remove();
-});
-
-buttonTheme.addEventListener("click", () => {
-    body.classList.toggle("body__dark");
-
-    changeTheme.innerText = body.classList.contains("body__dark") ? "dzienny" : "nocny";
-});
+    const onHeaderRemoveClic = () => {
+        header.remove()
+    }
+    
+    welcome();
+    
+    let buttonHeader = document.querySelector(".js-buttonHeader");
+    let header = document.querySelector(".js-header");
+    let buttonTheme = document.querySelector(".js-buttonTheme");
+    let body = document.querySelector(".body");
+    let  changeTheme = document.querySelector(".js-changeTheme");
+    
+    buttonHeader.addEventListener("click", onHeaderRemoveClic);
+    
+    buttonTheme.addEventListener("click", onChangeThemeClic);
+}
